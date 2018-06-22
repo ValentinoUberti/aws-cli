089c2e7be95818248987754309b3a9d0fb063c7f
Vale <=>
2018-06-22 17:03:18 +0200

    test5

---
+++ scripts/send_email.py
@@ -0,0 +1,19 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+ 
+
+server = smtplib.SMTP(host='mxout.appuio.ch',port=25)
+
+# create message object instance
+msg = MIMEMultipart()
+message = "Thank you"
+# setup the parameters of the message
+msg['From'] = "info@valentinouberti.com"
+msg['To'] = "info@valentinouberti.com"
+msg['Subject'] = "Test email from appuio"
+# add in the message body
+msg.attach(MIMEText(message, 'plain'))
+
+server.sendmail(msg['From'], msg['To'], msg.as_string())
+server.quit()
