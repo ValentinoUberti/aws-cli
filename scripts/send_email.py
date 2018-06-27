2cac45506ca2fba220d4767e8ec31269e56fe39d
Valentino Uberti <info@valentinouberti.com>
2018-06-27 08:52:43 +0200

    Update send_email.py

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -3,17 +3,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
  
-server = smtplib.SMTP(host='mxout.appuio.ch',port=25)
+server = smtplib.SMTP(host='<your-smtp-server>',port=25)
 
 # create message object instance
 msg = MIMEMultipart()
 message = "Thank you"
 # setup the parameters of the message
-msg['From'] = "info@valentinouberti.com"
-msg['To'] = "info@valentinouberti.com"
-msg['Subject'] = "Test email from appuio"
+msg['From'] = "<sender-email>"
+msg['To'] = "<receiver-email>"
+msg['Subject'] = "Test email"
 # add in the message body
 msg.attach(MIMEText(message, 'plain'))
-
 server.sendmail(msg['From'], msg['To'], msg.as_string())
 server.quit()
